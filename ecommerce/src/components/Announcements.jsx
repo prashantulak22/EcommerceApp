f2d3475cf1363f6eab3e3fa2cbc91f09dd1c7c70
Prashant <prashant9843215397@gmail.com>
2022-08-31 12:08:22 +0545

    Completion of Navbar

---
+++ ecommerce/src/components/Announcements.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+const Container = styled.div`
+      height: 40px;
+      background-color: teal;
+      color: white;
+      display: flex;
+      align-items: center;
+      font-size:18px;
+      justify-content: center;
+
+`
+
+const Announcements = () => {
+  return (
+    <Container>Get 50% off in Aaron brands</Container>
+  )
+}
+
+export default Announcements
---
+++ ecommerce/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Announcements from "../components/Announcements";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Announcements />
+      <Navbar />
+    </>
+  );
+};
+
+export default Home;
